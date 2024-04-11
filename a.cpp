ac895fad5d409024d0342ebfb1d4ba34eecde9bf
Makusoku <marcosfrisancho123@gmail.com>
2024-04-11 16:34:41 -0500

    cirsbot

--- a.cpp
+++ a.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 typedef long long ll;
 
-void solve() { cout << "Como odio todo"; };
+void solve() {
+  ll n, a, b;
+  cin >> n >> a >> b;
+  if (2 * a > b) {
+    cout << ((n / 2) * b) + ((n % 2) * a) << '\n';
+  } else {
+    cout << n * a << '\n';
+  }
+}
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
   ll t = 1;
+  cin >> t;
   while (t--)
     solve();
   return 0;
