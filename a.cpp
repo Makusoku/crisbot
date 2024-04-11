5b5ba048d2d16532e8f69900b05f84cb96bede83
Marcos Alonso Frisancho Machaca <marcos@Marcoss-MacBook-Air.local>
2024-04-11 08:01:45 -0500

    enseñando a crisbot a usar github

---
+++ a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  ll t = 1;
+  while (t--)
+    solve();
+  return 0;
+}
